1ecdae3226964b3052bfa0f8e267059756f7bfe8
Vitor Pellegrino <vitorp@gmail.com>
2011-07-13 21:05:15 -0300

    Configuration spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper' 
 
 describe Ruhappy do
-
-  before do
-    Ruhappy.configure do |config| 
-      config.pivotal_tracker_token   = '1234' 
-      config.pivotal_tracker_project = '5678'
+  context "configuring" do 
+    before do
+      Ruhappy.configure do |config| 
+        config.pivotal_tracker_token   = '1234' 
+        config.pivotal_tracker_project = '5678'
+      end 
     end 
+
+    its("configuration.pivotal_tracker_token")   { should == '1234' } 
+    its("configuration.pivotal_tracker_project") { should == '5678' } 
   end 
-
-  subject { Ruhappy } 
-
-  its("configuration.pivotal_tracker_token")   { should == '1234' } 
-  its("configuration.pivotal_tracker_project") { should == '5678' } 
 end 
+
+
+
 
