3debf0ae3995eb22ed58fd198d18f4738a6446ad
Vitor Pellegrino <vitorp@gmail.com>
2011-07-18 21:08:38 -0300

    Fixing app tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,13 +3,24 @@ require 'spec_helper'
 describe Ruhappy::App do
   context "GET /stories should fetch every pivotal tracker story" do
     before do 
-      Ruhappy::Story.stub_chain(:completed_stories, :to_json).and_return "myawesomecompletestories" 
+      Ruhappy::Story.stub!(:completed_stories).and_return([story_mock(name: "Foo", description: "Bar")]) 
       visit '/stories'
     end 
 
-    it "should return found stories converted to json" do
-      page.should have_content "myawesomecompletestories"
+    it "should display found stories" do
+      page.should have_css("h3.header", "Are u Happy?")
+
+      within(".stories") do 
+        page.should have_css(".name",         "Foo")
+        page.should have_css(".description",  "Bar")
+      end 
     end   
 
   end 
+end 
+
+
+private 
+def story_mock(stubs={})
+  @story_mock ||= mock(PivotalTracker::Story, stubs).as_null_object
 end 
